239e40c08e1efa6d762298040fd30483ce5891e6
Roman <i@zangetsu.ru>
2022-09-10 18:44:51 +0500

    Task_41

---
+++ Task_41/Program.cs
@@ -0,0 +1,26 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write($"Введи число М (количество чисел): ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] NumberOfNumbers = new int[size];
+
+void InputNumbers(int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Введи {i + 1}-ое число: ");
+        NumberOfNumbers[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+void CountPositiveNumber(int[] array, int count = 0)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count += 1;
+    }
+    Console.WriteLine($"Чисел больше ноля {count}");
+}
+
+InputNumbers(size);
+CountPositiveNumber(NumberOfNumbers);
