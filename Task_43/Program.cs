201ec8d2c0c79e158b42d400d48b69e6bdec0fff
Roman <i@zangetsu.ru>
2022-09-22 18:23:09 +0500

    TaskUp

--- Task_43/Program.cs
+++ Task_43/Program.cs
@@ -1,24 +1,39 @@
 ﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
 // значения b1, k1, b2 и k2 задаются пользователем.
 
-void IntersectionPoint()
+void Task43()
 {
     Console.Write("Введите b1: ");
     double b1 = Convert.ToDouble(Console.ReadLine());
+
     Console.Write("Введите k1: ");
     double k1 = Convert.ToDouble(Console.ReadLine());
+
     Console.Write("Введите b2: ");
     double b2 = Convert.ToDouble(Console.ReadLine());
+
     Console.Write("Введите k2: ");
     double k2 = Convert.ToDouble(Console.ReadLine());
 
-    double x = (b2 - b1) / (k1 - k2);
-    double y = k2 * x + b2;
-
-    x = Math.Round(x, 2);
-    y = Math.Round(y, 2);
-
-    Console.WriteLine($"Пересечение двух прямых в точке: ({x}; {y})");
+    IntersectionPoint(b1, k1, b2, k2);
 }
 
-IntersectionPoint();
+void IntersectionPoint(double b1, double k1, double b2, double k2)
+{
+    if ((b2 - b1) == 0 || (k1 - k2) == 0)
+    {
+        Console.WriteLine($"Делить на ноль нельзя {(b2 - b1)} / {(k1 - k2)}");
+    }
+    else
+    {
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k2 * x + b2;
+
+        x = Math.Round(x, 2);
+        y = Math.Round(y, 2);
+        Console.WriteLine($"Пересечение двух прямых в точке: ({x}; {y})");
+    }
+
+}
+
+Task43();
